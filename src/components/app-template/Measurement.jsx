7db9edd50a5a9107382ec61c6269539c1fc6f238
Apex <arpana036@gmail.com>
2025-01-16 01:09:14 +0530

    little bit of responsiveness

--- src/components/app-template/Measurement.jsx
+++ src/components/app-template/Measurement.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Measurement = (props) => {
   return (
-    <span  style={{position:"absolute",border:"1px solid rgba(178, 175, 175, 0.826)",height:"40%",width:"10%",right:"8%",top:"25%",borderRadius:"50px",display:"flex",justifyContent:"center"}} > <div style={{height:"25%",width:"70%",borderRadius:"50%",position:"inherit",background:"linear-gradient(120deg,blue,rgb(194, 77, 240))",bottom:`${props.bottom>=4?props.bottom:4}%`,transition:"0.3s ease-in-out"}}></div> </span>
+    <span className='measurement' style={{position:"absolute",border:"1px solid rgba(178, 175, 175, 0.826)",height:"40%",width:"10%",right:"8%",top:"25%",borderRadius:"50px",display:"flex",justifyContent:"center"}} > <div style={{height:"25%",width:"70%",borderRadius:"50%",position:"inherit",background:"linear-gradient(120deg,blue,rgb(194, 77, 240))",bottom:`${props.bottom>=4?props.bottom:4}%`,transition:"0.3s ease-in-out"}}></div> </span>
   )
 }
 
