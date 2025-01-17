b303c42404a68901d50d1f577deac765c49c8e8c
Apex <arpana036@gmail.com>
2025-01-17 19:56:07 +0530

    display while loading

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,17 +1,19 @@
-import {React} from 'react'
+import {React,useState} from 'react'
 import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 
 
 const Login = (props) => {
     const nav=useNavigate()
+    const [issubmitting,setIssubmitting]=useState(false)
     const {register,handleSubmit}=useForm()
     const onsubmit=async(data)=>{
+        setIssubmitting(true)
          const res=await fetch(`https://weather-backend-f0he.onrender.com/login`,{method:"POST",headers:{
           "Content-type":"application/json"
          },body:JSON.stringify(data)})
          const r=await res.json()
-
+         setIssubmitting(false)
          if(r.loggin){
          if(r.token){
            localStorage.setItem("weatherauthtoken",r.token)
@@ -30,6 +32,7 @@ const Login = (props) => {
   return (
     <>
     <div className="logcontainer">
+   {issubmitting && <div style={{position:"absolute",top:"30%",left:"40%"}}>Loading...</div>}
     <form className='weather-sign weather-log' onSubmit={handleSubmit(onsubmit)}>
       <div className="sheet"></div>
       <div className="wrap"><label htmlFor="email"><b>Email</b></label>
