ab5198cb50cfc79f382ec537618a0ec57b0c4b9c
EC2 Default User <ec2-user@ip-172-31-32-22.eu-north-1.compute.internal>
2025-07-03 19:31:26 +0000

    url changed

--- src/components/app-template/Additional.jsx
+++ src/components/app-template/Additional.jsx
@@ -44,7 +44,7 @@ const Additional = (props) => {
     },[])
 
     const forecast=async(place)=>{
-      const res=await fetch(`https://7cc2-3-110-46-34.ngrok-free.app/forecast`,{method:"POST",headers:{
+      const res=await fetch(`http://13.60.43.155:5005/forecast`,{method:"POST",headers:{
         "Content-type":"application/json"
       },body:JSON.stringify({place})})
       const obj=await res.json()
